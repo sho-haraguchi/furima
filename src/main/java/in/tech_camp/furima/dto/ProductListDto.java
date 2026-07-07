9c07630dce74392bc4d8fb8f96d03a3f856acb53
akira <a.ono@infomart.co.jp>
2026-07-07 17:23:09 +0900

    商品一覧表示のDtoを作成

---
+++ src/main/java/in/tech_camp/furima/dto/ProductListDto.java
@@ -0,0 +1,14 @@
+package in.tech_camp.furima.dto;
+
+import lombok.Data;
+
+@Data // 商品一覧表示
+public class ProductListDto {
+  
+  private String img;
+  private String name;
+  private Long price;
+  private int deliveryFee;
+  private int soldout;
+
+}
