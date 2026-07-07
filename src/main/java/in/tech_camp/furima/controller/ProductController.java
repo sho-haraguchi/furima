dfd46f4b7c20796158be638762c1bd185fc0b492
akira <a.ono@infomart.co.jp>
2026-07-07 17:54:59 +0900

    商品一覧画面の表示のcontrollerの記述

--- src/main/java/in/tech_camp/furima/controller/ProductController.java
+++ src/main/java/in/tech_camp/furima/controller/ProductController.java
@@ -1,8 +1,14 @@
 package in.tech_camp.furima.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProductController {
-  
+
+  @GetMapping({ "/", "", "items" })
+  public String showAllProduct() {
+    return "items/index";
+  }
+
 }
