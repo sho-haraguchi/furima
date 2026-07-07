aeff708ddd4f4c6520979dddab0ed4d8c230211b
sho-haraguchi <sho.haraguchi@infomart.co.jp>
2026-07-07 09:34:20 +0900

    Initial commit

---
+++ src/main/java/in/tech_camp/furima/FurimaApplication.java
@@ -0,0 +1,13 @@
+package in.tech_camp.furima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurimaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/tech_camp/furima/FurimaApplicationTests.java
@@ -0,0 +1,13 @@
+package in.tech_camp.furima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
