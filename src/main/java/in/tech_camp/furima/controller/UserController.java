c8614f4bf690c703193bb7e0a446c44d10656849
TA0608 <t.amano@infomart.co.jp>
2026-07-08 11:01:44 +0900

    正しい日付かどうか

--- src/main/java/in/tech_camp/furima/controller/UserController.java
+++ src/main/java/in/tech_camp/furima/controller/UserController.java
@@ -1,9 +1,5 @@
 package in.tech_camp.furima.controller;
 
-import in.tech_camp.furima.form.RegisterForm;
-import in.tech_camp.furima.service.UserService;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,6 +7,11 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import in.tech_camp.furima.form.RegisterForm;
+import in.tech_camp.furima.service.UserService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class UserController {
@@ -34,9 +35,18 @@ public class UserController {
 
     @PostMapping("/users/sign_up")
     public String userInsert(@Validated @ModelAttribute("registerForm") RegisterForm registerForm,
-                             BindingResult result, HttpServletRequest request) {
+            BindingResult result, HttpServletRequest request) {
 
         if (result.hasErrors()) {
+            errorSetPassForm(registerForm);
+            return "users/sign_up";
+        }
+        try {
+            java.time.LocalDate.of(registerForm.getBirthYear(), registerForm.getBirthMonth(),
+                    registerForm.getBirthDay());
+        } catch (java.time.DateTimeException e) {
+            // 存在しない日付の場合はエラーとして画面に戻す
+            result.rejectValue("birthDay", "error.birthDay", "存在する正しい日付を選択してください");
             errorSetPassForm(registerForm);
             return "users/sign_up";
         }
