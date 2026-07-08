f6443b10d9db09dacafe42037569a1c585a1b20b
akira <a.ono@infomart.co.jp>
2026-07-08 14:32:40 +0900

    UserEntityの作成

---
+++ src/main/java/in/tech_camp/furima/entity/UserEntity.java
@@ -0,0 +1,20 @@
+package in.tech_camp.furima.entity;
+
+import java.time.LocalDate;
+
+import lombok.Data;
+
+@Data
+public class UserEntity {
+  
+  private Long id;
+  private String nickname;
+  private String email;
+  private String password;
+  private String lastName;
+  private String firstName;
+  private String lastNameKana;
+  private String firstNameKane;
+  private LocalDate birthday;
+
+}
