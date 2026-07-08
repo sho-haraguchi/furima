81a3b8ff153464f9788b29ca6fe1afa65cfcfd91
tskiri <t.kiribayashi@infomart.co.jp>
2026-07-08 16:40:51 +0900

    Service, Controller（暫定）

--- src/main/java/in/tech_camp/furima/controller/OrderController.java
+++ src/main/java/in/tech_camp/furima/controller/OrderController.java
@@ -1,8 +1,87 @@
 package in.tech_camp.furima.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import in.tech_camp.furima.dto.OrderDto;
+import in.tech_camp.furima.entity.ProductEntity;
+import in.tech_camp.furima.service.OrderService;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @Controller
+@RequiredArgsConstructor
 public class OrderController {
   
+  private final OrderService orderService;
+
+  @Value("${payjp.public.key}")
+  private String payjpPublicKey;
+
+  // 購入画面の表示
+  @GetMapping("/items/{productId}/orders")
+  public String showOrderPage(@PathVariable Long productId, 
+                              @AuthenticationPrincipal CustomUserDetail userDetail,
+                              @ModelAttribute OrderDto orderDto, Model model) {
+
+    // ログアウト状態の場合は、商品購入ページに遷移しようとすると、商品の販売状況に関わらずログインページに遷移すること。
+    if (userDetail == null) {
+      return "redirect:/users/sign_in";
+    }
+
+    Long currentUserId = userDetail.getUser().getId();
+    ProductEntity product = orderService.findById(productId);
+
+    // // ログイン状態の場合でも、自身が出品した商品の商品購入ページに遷移しようとすると、商品の販売状況に関わらずトップページに遷移すること。
+    if (product.getUserid().equals(currentUserId)) {
+      return "redirect:/";
+    }
+
+    // ログイン状態の場合でも、自身が出品していない売却済み商品の商品購入ページへ遷移しようとすると、トップページに遷移すること。
+    if (orderService.isSoldout(productId)) {
+      return "redirect:/";
+    }
+
+    model.addAttribute("product", product);
+    model.addAttribute("payjpPublicKey", payjpPublicKey);
+    
+    return "orders/index";
+  }
+  
+
+  // 購入処理
+  @PostMapping("/items/{productId}/orders")
+  public String buy(@PathVariable Long productId,
+                    @AuthenticationPrincipal CustomUserDetail userDetail,
+                    @Validated(OrderDto.ValidationOrder.class) @ModelAttribute OrderDto orderDto,
+                    BindingResult bindingResult,
+                    Model model) {
+
+    // エラーハンドリングができること（入力に問題がある状態で「購入」ボタンが押された場合、情報は受け入れられず、購入ページでエラーメッセージが表示されること）。
+    if (bindingResult.hasErrors()) {
+      ProductEntity product = orderService.findById(productId);
+      model.addAttribute("product", product);
+      model.addAttribute("payjpPublicKey", payjpPublicKey); // 再表示時も鍵が必要なので
+
+      return "orders/index";
+    }
+
+    // OrderServiceの購入・決済処理
+    Long currentUserId = userDetail.getUser().getId();
+    orderService.createOrder(productId, currentUserId, orderDto);
+
+    return "redirect:/";
+  }
 }
--- src/main/java/in/tech_camp/furima/service/OrderService.java
+++ src/main/java/in/tech_camp/furima/service/OrderService.java
@@ -1,8 +1,57 @@
 package in.tech_camp.furima.service;
 
+import in.tech_camp.furima.dto.OrderDto;
+import in.tech_camp.furima.entity.AddressEntity;
+import in.tech_camp.furima.entity.BuyEntity;
+import in.tech_camp.furima.entity.ProductEntity;
+import in.tech_camp.furima.repository.AddressRepository;
+import in.tech_camp.furima.repository.BuyRepository;
+import in.tech_camp.furima.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class OrderService {
-  
+
+    private final ProductRepository productRepository;
+    private final BuyRepository buyRepository;
+    private final AddressRepository addressRepository;
+    private final PayjpService payjpService;
+
+    public ProductEntity findById(Long productId) {
+      return productRepository.findById(productId)
+                              .orElseThrow(() -> new IllegalArgumentException("Invalid product ID:" + productId));
+    }
+
+    public boolean isSoldout(Long productId) {
+      return buyRepository.existsByProductId(productId);
+    }
+
+    @Transactional
+    public void createOrder(Long productId, Long userId, OrderDto orderDto) {
+      ProductEntity product = findById(productId);
+
+      // PayjpServiceを使って決済処理
+      payjpService.charge(product.getPrice(), orderDto.getToken());
+
+      // buysテーブルへ保存
+      BuyEntity buy = new BuyEntity();
+      buy.setUserId(userId);
+      buy.setProductId(productId);
+      BuyEntity savedBuy = buyRepository.insert(buy);
+
+      // addressesテーブルへ保存
+      AddressEntity address = new AddressEntity();
+      address.setBuyId(savedBuy.getId());
+      address.setPostNumber(orderDto.getPostNumber());
+      address.setPrefecture(orderDto.getPrefecture());
+      address.setCity(orderDto.getCity());
+      address.setBlock(orderDto.getBlock());
+      address.setBuilding(orderDto.getBuilding());
+      address.setPhone(orderDto.getPhone());
+
+      addressRepository.save(address);
+    }
 }
