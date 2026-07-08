3d3bc9cd2ff7336b04c1cb2dbf0b4d7a12490389
akira <a.ono@infomart.co.jp>
2026-07-08 17:03:29 +0900

    商品詳細のMapperとDtoの作成

--- src/main/java/in/tech_camp/furima/repository/ProductRepository.java
+++ src/main/java/in/tech_camp/furima/repository/ProductRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import in.tech_camp.furima.dto.repository.ProductDetailQueryResult;
 import in.tech_camp.furima.dto.repository.ProductQueryResult;
 
 @Mapper
@@ -18,5 +19,17 @@ public interface ProductRepository {
       ORDER BY p.id DESC
       """)
   List<ProductQueryResult> findAll();
+
+  // 商品詳細
+  @Select("""
+      SELECT 
+        p.id,p.img,p.name,p.price,p.delivery_fee,p.description,
+        u.nickname,p.category,p.status,p.prefectures,p.untilDelivery,p.soldout
+      FROM products p
+      LEFT JOIN users u
+      ON p.userId = u.id
+      WHERE p.id = #{id}
+      """)
+  List<ProductDetailQueryResult> selectByProductId(Long id);
 
 }
