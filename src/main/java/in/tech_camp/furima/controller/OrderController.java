891a285c33bffdb9d4ae9f03bc7cda7bf0086f80
tskiri <t.kiribayashi@infomart.co.jp>
2026-07-08 17:03:33 +0900

    インポート文

--- src/main/java/in/tech_camp/furima/controller/OrderController.java
+++ src/main/java/in/tech_camp/furima/controller/OrderController.java
@@ -1,5 +1,6 @@
 package in.tech_camp.furima.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +8,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
