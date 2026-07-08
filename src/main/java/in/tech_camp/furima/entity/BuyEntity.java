6e7ac01d856ef12fc13fcc29b4491853554b0fa9
tskiri <t.kiribayashi@infomart.co.jp>
2026-07-08 16:39:38 +0900

    Entity

---
+++ src/main/java/in/tech_camp/furima/entity/BuyEntity.java
@@ -0,0 +1,10 @@
+package in.tech_camp.furima.entity;
+
+import lombok.Data;
+
+@Data
+public class BuyEntity {
+  private Long id;
+  private Long userId;
+  private Long productId;
+}
---
+++ src/main/java/in/tech_camp/furima/entity/ProductEntity.java
@@ -0,0 +1,17 @@
+package in.tech_camp.furima.entity;
+import lombok.Data;
+
+@Data
+public class ProductEntity {
+  private Long id;
+  private Long userId;
+  private String name;
+  private String description;
+  private Integer category;
+  private Integer condition;
+  private Integer deliveryFee;
+  private Integer prefecture;
+  private Integer untilDelivery;
+  private Long price;
+  private String img;
+}
