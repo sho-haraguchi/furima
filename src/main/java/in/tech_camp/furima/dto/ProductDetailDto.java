927783086e8370d1b53cd06a13aabe56a8bc8624
akira <a.ono@infomart.co.jp>
2026-07-09 11:39:28 +0900

    DetailDtoはEnumの型に修正

--- src/main/java/in/tech_camp/furima/dto/ProductDetailDto.java
+++ src/main/java/in/tech_camp/furima/dto/ProductDetailDto.java
@@ -1,5 +1,6 @@
 package in.tech_camp.furima.dto;
 
+import in.tech_camp.furima.enums.DeliveryFeeType;
 import lombok.Data;
 
 @Data // 商品詳細
@@ -8,7 +9,7 @@ public class ProductDetailDto {
   private String name;
   private String img;
   private Long price;
-  private int deliveryFee;
+  private DeliveryFeeType deliveryFee;
   private String description;
   private String nickname;
   private String category;
