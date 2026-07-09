1a1f4ecab49a405eb6b72fdd29f4efed22d41921
akira <a.ono@infomart.co.jp>
2026-07-09 11:39:12 +0900

    商品詳細、商品削除、編集のControllerを記述(修正点有)

--- src/main/java/in/tech_camp/furima/controller/ProductController.java
+++ src/main/java/in/tech_camp/furima/controller/ProductController.java
@@ -3,8 +3,12 @@ package in.tech_camp.furima.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import in.tech_camp.furima.service.ProductService;
+
 
 @Controller
 public class ProductController {
@@ -15,10 +19,36 @@ public class ProductController {
     this.productService = productService;
   }
 
+  // 商品一覧表示
   @GetMapping({ "/", "", "items" })
   public String showAllProduct(Model model) {
-    model.addAttribute("items",productService.allProduct());
+    model.addAttribute("items", productService.allProduct());
     return "items/index";
   }
+
+  // 商品詳細
+  @GetMapping("/items/{id}")
+  public String showProductDetail(@PathVariable Long id, Model model) {
+
+    model.addAttribute("item", productService.selectByProductId(id));
+
+    return "items/show";
+  }
+
+  // 商品削除
+  @PostMapping("/items/delete/{id}")
+  public String postMethodName(@PathVariable Long id, Model model) {
+
+    productService.deleteByProductId(id);
+
+    return "redirect:/";
+  }
+
+  // 商品編集
+  @GetMapping("path")
+  public String getMethodName(@RequestParam String param) {
+      return new String();
+  }
+  
 
 }
