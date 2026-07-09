83166cf8a93e8b71c74ca52694029bbb6849ff3a
akira <a.ono@infomart.co.jp>
2026-07-09 11:36:43 +0900

    商品削除と編集のMapperの作成

--- src/main/java/in/tech_camp/furima/repository/ProductRepository.java
+++ src/main/java/in/tech_camp/furima/repository/ProductRepository.java
@@ -2,10 +2,12 @@ package in.tech_camp.furima.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
-import in.tech_camp.furima.dto.repository.ProductDetailQueryResult;
+import in.tech_camp.furima.dto.ProductDetailDto;
 import in.tech_camp.furima.dto.repository.ProductQueryResult;
 
 @Mapper
@@ -22,14 +24,29 @@ public interface ProductRepository {
 
   // 商品詳細
   @Select("""
-      SELECT 
-        p.id,p.img,p.name,p.price,p.delivery_fee,p.description,
-        u.nickname,p.category,p.status,p.prefectures,p.untilDelivery,p.soldout
+      SELECT
+        p.name,p.img,p.price,p.delivery_fee,p.description,
+        u.nickname,p.category,p.status,p.prefectures,p.until_delivery,b.product_id
+        CASE WHEN b.product_id IS NOT NULL THEN 1 ELSE 0 END AS soldout
       FROM products p
-      LEFT JOIN users u
-      ON p.userId = u.id
+      LEFT JOIN users u ON p.user_id = u.id
+      LEFT JOIN buys b ON p.id = b.product_id
       WHERE p.id = #{id}
       """)
-  List<ProductDetailQueryResult> selectByProductId(Long id);
+  ProductDetailDto selectByProductId(Long id);
+
+  // 削除
+  @Delete("DELETE FROM products WHERE id = #{id}")
+  int deleteByProductId(Long id);
+
+  // 更新
+  @Update("""
+      UPDATE products 
+      SET img = #{img},name = #{name},description = #{description},
+      category = #{category},status = #{status},delivery_fee = #{deliveryFee},
+      prefectures = ${prefectures},until_delivery = #{untilDelivery},price = #{price}
+      WHERE id = #{id}
+      """)
+  int updateByProductId(Long id);
 
 }
