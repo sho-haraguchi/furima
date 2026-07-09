2d55c828098eb11b1e0c7627f308e4e3f802e232
akira <a.ono@infomart.co.jp>
2026-07-09 11:40:41 +0900

    商品詳細と削除、更新のServiceを記述。 詳細と更新に関してはEnumを使用するため変更予定有

--- src/main/java/in/tech_camp/furima/service/ProductService.java
+++ src/main/java/in/tech_camp/furima/service/ProductService.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import in.tech_camp.furima.dto.ProductDetailDto;
 import in.tech_camp.furima.dto.ProductListDto;
 import in.tech_camp.furima.enums.DeliveryFeeType;
 import in.tech_camp.furima.repository.ProductRepository;
@@ -34,6 +35,47 @@ public class ProductService {
         }).collect(Collectors.toList());
 
     return products;
+
+  }
+
+  // 詳細表示
+  public ProductDetailDto selectByProductId(Long id) {
+
+    ProductDetailDto dto = productRepository.selectByProductId(id);
+
+    dto.setDeliveryFee(DeliveryFeeType.fromCode(dto.getDeliveryFee().getCode()));
+
+    return dto;
+
+  }
+
+  // 商品削除
+  public int deleteByProductId(Long id) {
+
+    int result = productRepository.deleteByProductId(id);
+
+    if (result <= 0) {
+      throw new RuntimeException("削除できませんでした");
+    } else if (result >= 2) {
+      throw new RuntimeException("予想された挙動とは異なったため削除できませんでした");
+    }
+
+    return result;
+
+  }
+
+  // 商品更新
+  public int updateByProductId(Long id) {
+
+    int result = productRepository.updateByProductId(id);
+
+    if (result <= 0) {
+      throw new RuntimeException("削除できませんでした");
+    } else if (result >= 2) {
+      throw new RuntimeException("予想された挙動とは異なったため削除できませんでした");
+    }
+
+    return result;
 
   }
 
