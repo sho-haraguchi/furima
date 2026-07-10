4d7e0173377d9115d28827294d1a2576297de579
akira <a.ono@infomart.co.jp>
2026-07-10 09:18:16 +0900

    dtoのEnumの型をString変更

--- src/main/java/in/tech_camp/furima/dto/ProductDetailDto.java
+++ src/main/java/in/tech_camp/furima/dto/ProductDetailDto.java
@@ -1,25 +1,22 @@
 package in.tech_camp.furima.dto;
 
-import in.tech_camp.furima.enums.Category;
-import in.tech_camp.furima.enums.Condition;
-import in.tech_camp.furima.enums.DeliveryFeeType;
-import in.tech_camp.furima.enums.PrefectureType;
-import in.tech_camp.furima.enums.UntilDelivery;
 import lombok.Data;
 
 @Data // 商品詳細
 public class ProductDetailDto {
 
+  private Long id;
   private String name;
   private String img;
   private Long price;
-  private DeliveryFeeType deliveryFee;
+  private String deliveryFee;
   private String description;
   private String nickname;
-  private Category category;
-  private Condition status;
-  private PrefectureType prefectures;
-  private UntilDelivery untilDelivery;
+  private String category;
+  private String condition;
+  private String prefecture;
+  private String untilDelivery;
   private boolean soldout;
+  private Long userId;
 
 }
