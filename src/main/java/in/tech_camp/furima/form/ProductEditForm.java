42e6e7d7a223e564c402705b77b74b43deda76bc
akira <a.ono@infomart.co.jp>
2026-07-10 15:24:50 +0900

    ProductEditFormの型を修正

--- src/main/java/in/tech_camp/furima/form/ProductEditForm.java
+++ src/main/java/in/tech_camp/furima/form/ProductEditForm.java
@@ -5,13 +5,14 @@ import lombok.Data;
 @Data
 public class ProductEditForm {
 
+  private Long id;
   private String img; // 画像
   private String name; // 商品名
   private String description; // 商品説明
   private int category; // カテゴリー
-  private int status; // 商品の状態
+  private int condition; // 商品の状態
   private int deliveryFee; // 配送料の負担
-  private int prefectures; // 発送元の地域
+  private int prefecture; // 発送元の地域
   private int untilDelivery; // 発送までの日数
   private Long price; // 価格
 }
